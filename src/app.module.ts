b32ce83e302fa536b639a3abbe4f0a67179ac1f7
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2024-01-20 22:20:24 +0900

    CMT-11 게스트 계정 작업 중, 프로젝트 환경 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,22 @@ import { DatabaseModule } from './database/database.module';
 import { CommitmentModule } from './commitment/commitment.module';
 import { CommentModule } from './comment/comment.module';
 import { CommitmentActivityModule } from './commitment-activity/commitment-activity.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, DatabaseModule, CommitmentModule, CommentModule, CommitmentActivityModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+    UserModule,
+    DatabaseModule,
+    CommitmentModule,
+    CommentModule,
+    CommitmentActivityModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
